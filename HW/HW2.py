8bc800a45a2b3e8fb7c82f08b0d1e1bbdd35eae8
JasonVann <jasonvanet@gmail.com>
2016-10-30 14:04:14 -0700

    Added HW2 Q2

--- HW/HW2.py
+++ HW/HW2.py
@@ -30,7 +30,6 @@ def load_data1(is_test):
     return (n,  res)
 
 def max_k(num_k, is_test):
-    import copy
     (n, data) = load_data1(is_test)
     #data0 = copy.deepcopy(data)
     num = num_k
@@ -92,8 +91,116 @@ def max_k(num_k, is_test):
     print 89, min_d, (p_min, q_min)
     return (head, cluster)
 
-(n, data) = load_data1(False)
-(head, cluster) = max_k(3, True)
-(head, cluster) = max_k(4, False)
+def load_data2(is_test):
+    if not is_test:
+        file_name = 'clustering_big.txt'
+    else:
+        file_name = 'HW2_test2.txt'
+        
+    lines = [line.strip('\r\n') for line in open(file_name)]
+
+    head  = lines[0].split('\t')
+    if len(head) == 1:
+        head = lines[0].split(' ')
+
+    (n, b) = head
+    (n, b) = (int(n), int(b))
+
+    res = {}
+    j = 1
+    for i in range(1, len(lines)):
+        if lines[i] == '':
+            continue
+        temp = lines[i].split('\t')
+        if len(temp) == 1:
+            temp = lines[i].split(' ')
+        #print 118, temp
+        temp = [a for a in temp if a != '']
+        #print 120, temp
+        temp = ''.join(temp)
+        res[j] = temp
+        j += 1
+        
+    return ((n, b),  res)
+
+
+
+    
+def max_k_big(num_k, is_test):
+    def cal_d(p, q):
+        res = int(p, 2) ^ int(q, 2)
+        d = bin(res).count('1')
+        return d
+            
+    ((n, b),  data) = load_data2(is_test)
+    #data0 = copy.deepcopy(data)
+    num = num_k
+    n0 = n
+    head = {}
+    cluster = {}
+    for i in range(1, n+1):
+        head[i] = i
+        cluster[i] = [i]
+    while n > num:
+        #while len(connected) > num:
+        min_d = None
+
+        for i in range(1, n0+1):
+            for j in range(i+1, n0+1):
+
+                if head[i] == head[j]:
+                    continue
+
+                d = cal_d(data[i], data[j])       
+                if min_d == None or min_d > d:
+                    min_d = d
+                    (p_min, q_min) = (i, j)
+                    #break
+        #print 157, head
+        #print 158, (p_min, q_min), min_d, d, head[p_min], head[q_min]
+        p_head = head[p_min]
+        q_head = head[q_min]
+        if len(cluster[p_head]) > len(cluster[q_head]):
+            a, b = p_head, q_head
+        else:
+            a, b = q_head, p_head
+
+        #print 166, cluster[a], cluster[b], a, b
+        #if cluster[a] != [-1]:
+        cluster[a].extend(cluster[b])
+        #else:
+        #    cluster[a] = [b]
+        for temp in cluster[b]:
+            head[temp] = a
+        cluster.pop(b)
+        #print 174, cluster[a], a, b
+        '''
+        if min_d >= 3:
+            break
+        '''
+        n -= 1
+
+    min_d = None
+    k = 0
+    #print data
+    for i in range(1, n0+1):
+        for j in range(i+1, n0+1):
+            if head[i] != head[j]:
+                d = cal_d(data[i], data[j])
+                if min_d == None or min_d > d:
+                    min_d = d
+                    (p_min, q_min) = (i, j)
+    
+    print 186, min_d, (p_min, q_min)
+    return (head, cluster)
+
+
+#(n, data) = load_data1(False)
+#(head, cluster) = max_k(3, True)
+#(head, cluster) = max_k(4, False)
+
+#((n, b), data) = load_data2(False)
+(head, cluster) = max_k_big(3, True)
+#(head, cluster) = max_k_big(2, False)
 
 print time.time() - start_time
